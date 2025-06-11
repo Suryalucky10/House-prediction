0a17ab7fa630c85a689a8b00cabb8f0e9de53ae7
Suryalucky10 <161465734+Suryalucky10@users.noreply.github.com>
2025-06-11 21:55:42 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from flask import Flask, render_template, request
-import pickle 
-import numpy as np
-app=Flask(__name__)
-with open('trained_mod.pkl','rb') as f:
-    rfg.pickle.load(f);
